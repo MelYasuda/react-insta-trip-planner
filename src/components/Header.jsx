7124ba35949886d2d826cd6afdfd8b0f82a4dd5d
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-12-07 13:59:02 -0800

    plan specifications of the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,44 @@ import React, { Component } from "react";
 
 function Header() {
   return (
-    <div className='container'>
-      <div className='jumbotron'>
-        Insta trip planner
+    <nav
+      id="top-navbar"
+      className="navbar navbar-expand-lg"
+    >
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a id="brand1" className="nav-link" href="">
+              <i className="fa fa-instagram" aria-hidden="true" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a id="brand2" className="nav-link" href="#">
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
-  );
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-compass" aria-hidden="true" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-heart-o" aria-hidden="true" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a id="cart" className="nav-link">
+              <i className="fa fa-user-o" aria-hidden="true" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>  );
 }
 
 export default Header;
